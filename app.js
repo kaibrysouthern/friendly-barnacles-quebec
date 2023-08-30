a2abce4dac23ebd1c2d6d12002bd97413f8cba22
Kaibry Southern <kaibrysouthern@Kaibrys-MacBook-Pro.local>
2023-08-29 22:01:20 -0500

    halfway

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs');
+
+let myVariableServer = "soft coded server data";
+
+app.get('/view', function (req, res) {
+  res.render('index', 
+  {
+    'myVariableClient': myVariableServer
+  });
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World From Express and a Paas/Render' )
 })
 
-app.listen(3000)
+app.get('/whatever', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+app.listen(4000);
