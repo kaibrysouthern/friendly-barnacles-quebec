d9dfdb05c214a577c0edcc8b5b6ea04a9ac4387e
Kaibry Southern <kaibrysouthern@Kaibrys-MacBook-Pro.local>
2023-08-31 13:38:34 -0500

    mirroring stuff

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let myVariableServer = "soft coded server data";
 
@@ -9,6 +11,19 @@ app.get('/view', function (req, res) {
   res.render('index', 
   {
     'myVariableClient': myVariableServer
+  });
+})
+
+app.post('/postClientData', function (req, res) {
+  console.log("body: ", req.body)
+   console.log("user Name: ", req.body.userName)
+  //  console.log("params: ", req.params['userName']);
+
+  //myVariableServer = "now we have posted"
+
+  res.render('index', 
+  {
+    'myVariableClient': req.body.userName 
   });
 })
 
